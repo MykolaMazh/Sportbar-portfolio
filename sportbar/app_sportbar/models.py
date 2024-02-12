5027a4cd109519a4caa2e9349d150cb2deef9e6f
MykolaMazhula <mykm3ua@gmail.com>
2024-02-12 21:52:39 +0100

    Add models(Category, MenuPosition)  and settings for media files in settings.py(MEDIA_ROOT, MEDIA_URL) and urls.py(if settings.DEBUG)

--- sportbar/app_sportbar/models.py
+++ sportbar/app_sportbar/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/%Y/%m/%d', max_length=100,
+                              blank=True)
+    description = models.CharField(max_length=150, blank=True)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
+
+
+class MenuPosition(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%Y/%m/%d', max_length=100,
+                              blank=True)
+
+    def __str__(self):
+        return self.title
