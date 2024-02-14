3323d3e4e88f87bf5943e95d87a0ddb35a722dce
MykolaMazhula <mykm3ua@gmail.com>
2024-02-14 21:11:50 +0100

    Register MenuPosition, Category in admin.py

--- sportbar/app_sportbar/admin.py
+++ sportbar/app_sportbar/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+class MenuPositionAdmin(admin.ModelAdmin):
+    save_as = True
+
+admin.site.register(MenuPosition, MenuPositionAdmin)
+admin.site.register(Category, CategoryAdmin)
