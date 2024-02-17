dc72e6ca620e32cd2beaacf7a49659856dd50529
MykolaMazhula <mykm3ua@gmail.com>
2024-02-17 21:29:42 +0100

    Change slider content in base.html

--- sportbar/app_sportbar/urls.py
+++ sportbar/app_sportbar/urls.py
@@ -4,6 +4,6 @@ from .views import index, CategoryDetailView
 app_name = "app_sportbar"
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="home"),
     path("category_detail/<str:slug>", CategoryDetailView.as_view(), name="category_detail"),
 ]
