fccb72920361f4a8691d4bbe7a68cb3aaad0660a
MykolaMazhula <mykm3ua@gmail.com>
2024-02-25 00:09:01 +0100

    App_cart: Add __iter__ method to model Cart to iterate cart by values

--- sportbar/app_cart/models.py
+++ sportbar/app_cart/models.py
@@ -36,3 +36,14 @@ class Cart:
         self.session[settings.CART_SESSION_ID] = self.cart
 
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        # getting products from model
+        products = MenuPosition.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
