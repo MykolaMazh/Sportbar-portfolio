e5abd0ad3cb21a335d2e2433718b0724d449aa14
MykolaMazhula <mykm3ua@gmail.com>
2024-02-25 00:05:07 +0100

    App_cart: Add cart.html

--- sportbar/app_cart/urls.py
+++ sportbar/app_cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 
-from app_cart.views import cart_add
+from app_cart.views import cart_add, cart
 
 app_name = 'cart'
 
 urlpatterns = [
+    path('', cart, name='cart'),
     path("add/<str:slug>/", cart_add, name="add-to-cart"),
 
 ]
--- sportbar/app_cart/views.py
+++ sportbar/app_cart/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
 from app_cart.forms import CartAddProductForm
 from app_cart.models import Cart
 from app_sportbar.models import MenuPosition
 
+
+def cart(request):
+    cart = Cart(request)
+    return render(request, "cart/cart.html", {"cart_instance":cart})
 
 
 def cart_add(request, slug):
@@ -13,6 +18,10 @@ def cart_add(request, slug):
         cart.add(
             product=MenuPosition.objects.get(slug=slug),
             **form.cleaned_data)
+        print(cart.cart)
+        for item in cart.cart.values():
+            print(item)
+        return redirect(request.META.get('HTTP_REFERER'))
 
 
 
