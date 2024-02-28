3ac13b6df8a7de376900a12b01f3cc71f4462f87
MykolaMazhula <mykm3ua@gmail.com>
2024-02-28 22:20:04 +0100

    App_sportbar: Add BookTableForm into forms.py

--- sportbar/app_sportbar/forms.py
+++ sportbar/app_sportbar/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from app_sportbar.models import BookedTable
+
 
 class ClientCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -14,3 +16,13 @@ class ClientCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ("username", "password1", "password2", "avatar")
+
+
+class BookTableForm(forms.ModelForm):
+    class Meta:
+        model = BookedTable
+        fields = ['datatime_booked', 'phone']
+        widgets = {
+            'datatime_booked': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'phone': forms.NumberInput(attrs={'type': 'tel'})
+        }
