bdb01956e91481e6ae92687d18acedac92783ef6
MykolaMazhula <mykm3ua@gmail.com>
2024-04-06 21:27:34 +0200

    Add STATIC_ROOT into settings.py

--- sportbar/sportbar/settings.py
+++ sportbar/sportbar/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -139,6 +140,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
