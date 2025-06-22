c87975fb9596505b62356e61228ddce6ef3f5667
MykolaMazhula <mykm3ua@gmail.com>
2025-06-22 17:24:03 +0200

    Add PoSTGREs for render.com

--- sportbar/build.sh
+++ sportbar/build.sh
@@ -2,6 +2,8 @@
 # Exit on error
 set -o errexit
 
+echo "Current working directory: $(pwd)"
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r ../requirements.txt
 
@@ -10,3 +12,4 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+export DATABASE_URL=postgresql://sportbar_db_postgre_owner:npg_xF3ljsVPdL4T@ep-wild-poetry-a2r3wplv-pooler.eu-central-1.aws.neon.tech/sportbar_db_postgre?sslmode=require
