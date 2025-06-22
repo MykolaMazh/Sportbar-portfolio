416383427a433834cbe34acc3566158c3f55a8a2
MykolaMazhula <mykm3ua@gmail.com>
2025-06-22 19:05:11 +0200

    Add PoSTGREs for render.com1

--- sportbar/build.sh
+++ sportbar/build.sh
@@ -3,17 +3,17 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install --prefer-binary -r /opt/render/project/requirements.txt
+pip install --prefer-binary -r ./requirements.txt
 
 pip install --upgrade setuptools wheel
 
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r ./requirements.txt
+#pip install -r ./requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python sportbar/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python sportbar/manage.py migrate
 export DATABASE_URL=postgresql://sportbar_db_postgre_owner:npg_xF3ljsVPdL4T@ep-wild-poetry-a2r3wplv-pooler.eu-central-1.aws.neon.tech/sportbar_db_postgre?sslmode=require
